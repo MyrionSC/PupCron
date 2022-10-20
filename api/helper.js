3c8ed3a9e6bef62682efdaea1f9f9dec1a43a5c5
Marand <marpht23@gmail.com>
2022-10-20 18:12:14 +0200

    write to logfile

--- api/helper.js
+++ api/helper.js
@@ -1,14 +1,24 @@
 const childProcess = require('child_process')
+const fs = require('fs');
+const {StringDecoder} = require('string_decoder');
 
 function runScript(cwd, script, callback) {
     // keep track of whether callback has been invoked to prevent multiple invocations
     let invoked = false;
-    const process = childProcess.fork(script, {cwd: cwd});
+    const process = childProcess.fork(script, {cwd: cwd, silent: true});
+    const stream = fs.createWriteStream(cwd + "/logs.txt", {flags: 'a', encoding: 'utf8'});
+    const decoder = new StringDecoder('utf8');
+
+    process.stdout.on('data', (data) => {
+        stream.write(data)
+        console.log(decoder.write(data))
+    });
 
     // listen for errors as they may prevent the exit event from firing
     process.on('error', function (err) {
         if (invoked) return;
         invoked = true;
+        stream.close()
         callback(err);
     });
 
@@ -16,8 +26,8 @@ function runScript(cwd, script, callback) {
     process.on('exit', function (code) {
         if (invoked) return;
         invoked = true;
-        var err = code === 0 ? null : new Error('exit code ' + code);
-        callback(err);
+        stream.close()
+        callback(code === 0 ? null : new Error('exit code ' + code));
     });
 }
 
