0c186eddfd5529b5589b392ab03945231b4ce5cc
Marand <marpht23@gmail.com>
2022-10-25 18:25:09 +0200

    cors

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(fileUpload({
 }));
 
 //add other middleware
-app.use(cors());
+app.use(cors({origin: ["http://localhost:3000", "https://cronpup.marand.dk"]}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
